5cb1b24b1a0f62a12d01c1ede33ee87e1078596c
Shiedayn <ali.alperen.uzun@gmail.com>
2023-10-29 18:24:55 +0300

    small change

--- Assets/Script/NumberFormatter.cs
+++ Assets/Script/NumberFormatter.cs
@@ -5,12 +5,28 @@ public class NumberFormatter : MonoBehaviour
     public static string FormatNumber(double value)
     {
         if (value >= 1000000000)
-            return (value / 1000000000.0).ToString("0.##B");
+        {
+            return (value / 1000000000.0 >= 100) ? 
+                (value / 1000000000.0).ToString("0B") : 
+                (value / 1000000000.0).ToString("0.#B");
+        }
         else if (value >= 1000000)
-            return (value / 1000000.0).ToString("0.##M");
+        {
+            return (value / 1000000.0 >= 100) ? 
+                (value / 1000000.0).ToString("0M") : 
+                (value / 1000000.0).ToString("0.#M");
+        }
         else if (value >= 1000)
-            return (value / 1000.0).ToString("0.##K");
+        {
+            return (value / 1000.0 >= 100) ? 
+                (value / 1000.0).ToString("0K") : 
+                (value / 1000.0).ToString("0.#K");
+        }
         else
-            return value.ToString("0.##");
+        {
+            return value >= 100 ? 
+                value.ToString("0") : 
+                value.ToString("0.#");
+        }
     }
 }
